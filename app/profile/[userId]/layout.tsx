44be57c03f09e64ed030acf82faf0ae844da4a53
emircorp <141232565+emircorp@users.noreply.github.com>
2025-06-07 21:00:53 +0300

    Create layout.tsx

---
+++ app/profile/[userId]/layout.tsx
@@ -0,0 +1,8 @@
+export default function ProfileLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div>
+      {/* Ortak layout veya stil */}
+      {children}
+    </div>
+  )
+}
